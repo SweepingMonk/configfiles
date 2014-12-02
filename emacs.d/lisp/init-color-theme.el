2db4451e270cdc0bbee54d3b1e789ce78f8b4870
SweepingMonk <wangxiang1124@gmail.com>
2014-12-02 22:48:30 +0800

    change the tmux terminal type from screen-256color to screen and change the tabbar face

--- emacs.d/lisp/init-color-theme.el
+++ emacs.d/lisp/init-color-theme.el
@@ -1,12 +1,15 @@
 ;; Configuration about color-theme
 (require-package 'color-theme)
+(require-package 'color-theme-molokai)
+(require-package 'color-theme-solarized)
 
 (require 'color-theme)
 (color-theme-initialize)
-(when (display-graphic-p)
-  ;; Download several color theme not included in color-theme package
-  (require-package 'color-theme-molokai)
-  (require-package 'color-theme-solarized)
-  (color-theme-solarized-dark))
+(color-theme-solarized-dark)
+;(when (display-graphic-p)
+;  ;; Download several color theme not included in color-theme package
+;  (require-package 'color-theme-molokai)
+;  (require-package 'color-theme-solarized)
+;  (color-theme-solarized-dark))
 
 (provide 'init-color-theme)
