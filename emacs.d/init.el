d48546f4f45ec7f2d7ffc7c7b02e2907fd1ef6df
SweepingMonk <wangxiang1124@gmail.com>
2015-04-16 10:18:31 +0800

    change the color theme configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -13,18 +13,3 @@
 (require 'init-tabbar)
 (require 'init-web)
 ;(require 'init-powerline)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(custom-safe-themes
-   (quote
-    ("8db4b03b9ae654d4a57804286eb3e332725c84d7cdab38463cb6b97d5762ad26" default)))
- '(tabbar-separator (quote (0.5))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
--- emacs.d/lisp/init-color-theme.el
+++ emacs.d/lisp/init-color-theme.el
@@ -6,14 +6,37 @@
 ;;(require 'color-theme)
 ;;(color-theme-initialize)
 
-(if (daemonp)
+(defun graphic-frame-action (frame)
+  (set-frame-parameter frame 'background-mode 'dark)
+  (set-frame-parameter frame 'fullscreen 'fullboth)
+  (set-frame-parameter frame 'font "Monaco-13")
+  (enable-theme 'solarized))
+
+(defun terminal-frame-action (frame)
+  (set-terminal-parameter frame 'background-mode 'dark)
+  (enable-theme 'solarized))
+
+(defun add-make-frame-hook ()
     (add-hook 'after-make-frame-functions
 	      (lambda (frame)
-		(progn (select-frame frame)
-		       (message "setting-custom-color-theme")
-		       (load-theme 'solarized t))))
-  (load-theme 'solarized t))
+		(if (display-graphic-p frame)
+		    (graphic-frame-action frame)
+		  (terminal-frame-action frame)))))
 
+(defun normal-action ()
+  (if (display-graphic-p nil) (graphic-frame-action nil) (terminal-frame-action nil))
+  (add-make-frame-hook))
+
+;; load the theme
+(load-theme 'solarized t)
+(if (daemonp) (add-make-frame-hook) (normal-action))
+;(add-hook 'after-make-frame-functions
+;	  (lambda (frame)
+;	    (progn (message "enable theme")
+;		   (set-frame-parameter frame 'background-mode 'dark)
+;		   (enable-theme 'solarized))))
+
+;(add-to-list 'default-frame-alist '(font . "Monaco-14"))
 ;(when (display-graphic-p)
 ;  ;; Download several color theme not included in color-theme package
 ;  (require-package 'color-theme-molokai)
