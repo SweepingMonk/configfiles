fb638cc226a91c52e8372282eae3fab971ce2e50
SweepingMonk <wangxiang1124@gmail.com>
2016-03-25 11:33:32 +0800

    change html indent offset in web mode

--- emacs.d/lisp/init-web.el
+++ emacs.d/lisp/init-web.el
@@ -17,7 +17,7 @@
 
 (add-hook 'web-mode-hook
 	  (lambda ()
-	    (setq web-mode-markup-indent-offset 2)
+	    (setq web-mode-markup-indent-offset 4)
 	    (setq web-mode-code-indent-offset 4)
 	    (setq web-mode-css-indent-offset 2)
 	    (setq web-mode-style-padding 2)
