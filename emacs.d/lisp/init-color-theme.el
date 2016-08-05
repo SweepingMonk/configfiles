bd0254b7abd4e6e55b29d2055f6fb31b00004a46
gzwangxiang <gzwangxiang@corp.netease.com>
2016-08-05 16:46:34 +0800

    refector color theme configuration

--- emacs.d/lisp/init-color-theme.el
+++ emacs.d/lisp/init-color-theme.el
@@ -4,69 +4,58 @@
 
 (require-package 'color-theme-solarized)
 
-;;(require 'color-theme)
-;;(color-theme-initialize)
-
 (defvar my-color-theme 'solarized)
 
-;;; font family setting
-(defvar default-font-family)
-(cond
- ((string-equal system-type "windows-nt") (setq default-font-family "Consolas"))
- ((string-equal system-type "darwin") (setq default-font-family "Monaco"))
- ((string-equal system-type "gnu/linux") (setq default-font-family "Liberation Mono")))
-(defvar prefer-font-family "Inconsolata")
-(defvar x-font-family (if (member prefer-font-family (font-family-list))
-			  prefer-font-family default-font-family ))
-
-;;; font size setting
-(defvar x-font-size 16)
-
 (defun graphic-frame-action (frame)
-  (set-fontset-font "fontset-default"
-		    'han
-		    (font-spec :family "Hiragino Sans GB" :size 14))
-  (set-fontset-font "fontset-default"
-		    'cjk-misc
-		    (font-spec :family "Hiragino Sans GB" :size 14))
-  (set-frame-parameter frame 'background-mode 'light)
-  (set-frame-parameter frame 'width 115)
-  (set-frame-parameter frame 'height 38)
-  (set-frame-parameter frame 'left 200)
-  (set-frame-parameter frame 'top 40)
-  (set-frame-parameter frame 'font (format "%s-%d" x-font-family x-font-size))
+  "Graphic frame action, FRAME is the selected frame."
+  (let* ((default-font-family (cond
+			       ((string-equal system-type "windows-nt") "Consolas")
+			       ((string-equal system-type "darwin") "Monaco")
+			       ((string-equal system-type "gnu/linux") "Liberation Mono")))
+	 (prefer-font-family "Inconsolata")
+	 (x-font-family (if (member prefer-font-family (font-family-list))
+			    prefer-font-family default-font-family))
+	 (x-font-size 16)
+	 (chinese-font-family (cond
+			       ((string-equal system-type "windows-nt") "Microsoft Yahei")
+			       ((string-equal system-type "darwin") "Hiragino Sans GB")
+			       ((string-equal system-type "gnu/linux") "Microsoft Yahei"))))
+    (set-fontset-font "fontset-default"
+		      'han
+		      (font-spec :family chinese-font-family :size 14))
+    (set-fontset-font "fontset-default"
+		      'cjk-misc
+		      (font-spec :family chinese-font-family :size 14))
+    (set-frame-parameter frame 'background-mode 'light)
+    (set-frame-parameter frame 'width 115)
+    (set-frame-parameter frame 'height 38)
+    (set-frame-parameter frame 'left 200)
+    (set-frame-parameter frame 'top 40)
+    (set-frame-parameter frame 'font (format "%s-%d" x-font-family x-font-size)))
   (enable-theme my-color-theme))
 
 (defun terminal-frame-action (frame)
+  "Terminal frame action, FRAME is the selected frame."
+  (set-frame-parameter frame 'background-mode 'dark)
   (set-terminal-parameter frame 'background-mode 'dark)
   (enable-theme my-color-theme))
 
 (defun add-make-frame-hook ()
-    (add-hook 'after-make-frame-functions
-	      (lambda (frame)
-		(if (display-graphic-p frame)
-		    (graphic-frame-action frame)
-		  (terminal-frame-action frame)))))
+  "When Emacs is in daemon mode, add make frame hook to do different action."
+  (add-hook 'after-make-frame-functions
+	    (lambda (frame)
+	      (if (display-graphic-p frame)
+		  (graphic-frame-action frame)
+		(terminal-frame-action frame)))))
 
 (defun normal-action ()
+  "When Emacs is in normal mode(opend by command), do actions then add make frame-hook."
   (if (display-graphic-p nil) (graphic-frame-action nil) (terminal-frame-action nil))
   (add-make-frame-hook))
 
 ;; load the theme
 (load-theme my-color-theme t)
 (if (daemonp) (add-make-frame-hook) (normal-action))
-;(add-hook 'after-make-frame-functions
-;	  (lambda (frame)
-;	    (progn (message "enable theme")
-;		   (set-frame-parameter frame 'background-mode 'dark)
-;		   (enable-theme 'solarized))))
-
-;(add-to-list 'default-frame-alist '(font . "Monaco-14"))
-;(when (display-graphic-p)
-;  ;; Download several color theme not included in color-theme package
-;  (require-package 'color-theme-molokai)
-;  (require-package 'color-theme-solarized)
-;  (color-theme-solarized-dark))
 
 (provide 'init-color-theme)
 ;;; init-color-theme.el ends here
