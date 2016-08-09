8c8c2c6229eddef53b957f435c995431a66a1201
gzwangxiang <gzwangxiang@corp.netease.com>
2016-08-09 17:37:27 +0800

    add evil-surround

--- emacs.d/lisp/init-evil.el
+++ emacs.d/lisp/init-evil.el
@@ -1,6 +1,14 @@
-;; configuration about evil (extended vi layer for emacs)
+;;; package --- Summary
+;;; Commentary:
+;; Configuration about evil (extended vi layer for Emacs)
+
+;;; Code:
 (require-package 'evil)
+(require-package 'evil-surround)
 (require 'evil)
+(require 'evil-surround)
 (evil-mode 1)
+(global-evil-surround-mode 1)
 
 (provide 'init-evil)
+;;; init-evil ends here
